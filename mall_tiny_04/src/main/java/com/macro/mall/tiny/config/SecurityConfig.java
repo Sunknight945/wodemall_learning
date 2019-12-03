3c20fd5416131810d71b7f82ad89f627ed9fcd17
Sunknight945 <uiysuiysuiysuiys@Gmail.com>
2019-12-04 06:06:53 +0800

    tiny04 添加了一个 JWTTokenUtils的工具类 生成 token(json web token) 获取token 获取里面的载荷  获取载荷的用户信息 UserDetails 判断token是否过期 刷新token ...

--- mall_tiny_04/src/main/java/com/macro/mall/tiny/config/SecurityConfig.java
+++ mall_tiny_04/src/main/java/com/macro/mall/tiny/config/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.macro.mall.tiny.config;
 
 
 import com.macro.mall.tiny.component.JwtAuthenticationTokenFilter;
+import com.macro.mall.tiny.component.RestAuthenticationEntryPoint;
+import com.macro.mall.tiny.component.RestfulAccessDeniedHandler;
+import com.macro.mall.tiny.dto.AdminUserDetails;
 import com.macro.mall.tiny.mbg.model.UmsAdmin;
 import com.macro.mall.tiny.mbg.model.UmsPermission;
 import com.macro.mall.tiny.service.UmsAdminService;
+import com.sun.deploy.security.UserDeclinedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +21,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -38,17 +41,58 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   private RestAuthenticationEntryPoint restAuthenticationEntryPoint;
 
- 
+
+//  @Override
+//  protected void configure(HttpSecurity http) throws Exception {
+//    http.csrf()
+//        .disable()
+//        .sessionManagement()  //基于token, 所以不需要session
+//        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//        .and()
+//        .authorizeRequests()
+//        .antMatchers(HttpMethod.GET,
+//            "/",
+//            "/favicon.ico",
+//            "/*.html",
+//            "/**/*.css",
+//            "/**/*.js",
+//            "/swagger-resources/**",
+//            "/v2/api-docs/**"
+//        )
+//        .permitAll()
+//        .antMatchers("/admin/login", "/admin/register") //对登录或者注册允许匿名访问
+//        .permitAll()
+//        .antMatchers(HttpMethod.OPTIONS) //跨域的请求会先进行一次options请求
+//        .permitAll()
+//        .antMatchers("/**") //测试时全部运行访问
+//        .permitAll()
+//        .anyRequest() //除了上面允许的请求路径 请求方法 和资源外, 所有的请求都需要经过鉴权认证
+//        .authenticated();
+//    //禁用缓存
+//    http.headers().cacheControl(); //禁用缓存
+//    // 添加jwt filter
+//    http.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+//    //添加自定义未授权和未登录结果返回
+//    http.exceptionHandling()
+//        .accessDeniedHandler(restfulAccessDeniedHandler)
+//        .authenticationEntryPoint(restAuthenticationEntryPoint);
+//  }
+  
+  
+  /**
+   * http.authorizeRequests().anyRequest().authenticated().and().formLogin().and().httpBasic();
+   */
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf()
         .disable()
-        .sessionManagement()  //基于token, 所以不需要session
+        .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
         .and()
         .authorizeRequests()
         .antMatchers(HttpMethod.GET,
             "/",
+            "/fa",
             "/favicon.ico",
             "/*.html",
             "/**/*.css",
@@ -57,60 +101,69 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/v2/api-docs/**"
         )
         .permitAll()
-        .antMatchers("/admin/login", "/admin/register") //对登录或者注册允许匿名访问
+        .antMatchers("/admin/login", "/admin/register")
         .permitAll()
-        .antMatchers(HttpMethod.OPTIONS) //跨域的请求会先进行一次options请求
+        .antMatchers(HttpMethod.OPTIONS)
         .permitAll()
-        .antMatchers("/**") //测试时全部运行访问
+        .antMatchers("/**")
         .permitAll()
-        .anyRequest() //除了上面允许的请求路径 请求方法 和资源外, 所有的请求都需要经过鉴权认证
+        .anyRequest()
         .authenticated();
-    //禁用缓存
-    http.headers().cacheControl(); //禁用缓存
-    // 添加jwt filter
-    http.addFilterBefore(jwtAuthenticationTokenFilter(),  UsernamePasswordAuthenticationFilter.class);
-    //添加自定义未授权和未登录结果返回
-    http.exceptionHandling()
-        .accessDeniedHandler()
-        .authenticationEntryPoint();
+    http.headers().cacheControl();
+    http.addFilterBefore(jwtAuthenticationTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    
   }
-
-
+  
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService())
         .passwordEncoder(passwordEncoder());
   }
-
-
+  
+  
   /**
    * spring security 安全 中的密码的编码规则不能少
-   * @return  编码规则
+   *
+   * @return 编码规则
    */
   @Bean
-  public PasswordEncoder passwordEncoder(){
+  public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-
-  @Bean
-  public UserDetailsService userDetailsService(){
+  
+ /* @Bean
+  public UserDetailsService userDetailsService() {
     //获取用户登录信息
-    return ->{
+    return username -> {
       UmsAdmin admin = adminService.getAdminByUsername(username);
       if (admin != null) {
         List<UmsPermission> permissionList = adminService.getPermissionList(admin.getId());
-        return new AdminUserDetails(admin,permissionList);
+        return new AdminUserDetails(admin, permissionList);
       }
       throw new UsernameNotFoundException("用户名和密码错误");
-    }
-  }
-
+    };
+  }*/
+  
   @Bean
-  public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter(){
+  public UserDetailsService userDetailsService() {
+    //获取用户登录信息
+    return username -> {
+      UmsAdmin umsAdmin = adminService.getAdminByUsername(username);
+      if (umsAdmin != null) {
+        List<UmsPermission> permissionList = adminService.getPermissionList(umsAdmin.getId());
+        return new AdminUserDetails(umsAdmin, permissionList);
+      }
+      throw new UserDeclinedException("用户名和密码错误");
+    };
+  }
+  
+  
+  @Bean
+  public JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter() {
     return new JwtAuthenticationTokenFilter();
   }
-
-
+  
+  
   @Override
   public AuthenticationManager authenticationManagerBean() throws Exception {
     return super.authenticationManagerBean();
--- mall_tiny_04/src/main/java/com/macro/mall/tiny/controller/UmsAdminController.java
+++ mall_tiny_04/src/main/java/com/macro/mall/tiny/controller/UmsAdminController.java
@@ -1,6 +1,7 @@
 package com.macro.mall.tiny.controller;
 
 import com.macro.mall.tiny.common.api.CommonResult;
+import com.macro.mall.tiny.dto.UmsAdminLoginParam;
 import com.macro.mall.tiny.mbg.model.UmsAdmin;
 import com.macro.mall.tiny.service.UmsAdminService;
 import io.swagger.annotations.Api;
@@ -18,23 +19,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "UmsAdminController", description = "后台用户管理")
 @RequestMapping("/admin")
 public class UmsAdminController {
-  /**
-   * jwt:
-   * tokenHeader: Authorization #JWT存储的请求头
-   * secret: mySecret #JWT加解密使用的密钥
-   * expiration: 604800 #JWT的超期限时间(60*60*24)
-   * tokenHead: Bearer  #JWT负载中拿到开头
-   */
   
   @Autowired
   UmsAdminService umsAdminService;
   @Value("${jwt.tokenHeader}")
   String tokenHeader;
-  @Value("${jwt.tokenHeader}")
+  @Value("${jwt.tokenHead}")
   String tokenHead;
   
-  @PostMapping("/register")
   @ApiOperation(value = "用户注册")
+  @PostMapping("/register")
   public CommonResult<UmsAdmin> register(UmsAdmin umsAdmin) {
     UmsAdmin newUmsAdmin = this.umsAdminService.register(umsAdmin);
     if (newUmsAdmin == null) {
@@ -43,6 +37,14 @@ public class UmsAdminController {
       return CommonResult.success(umsAdmin);
     }
   }
+  
+  
+  @ApiOperation("登录以后返回token")
+  @PostMapping("/login")
+  public CommonResult login(UmsAdminLoginParam umsAdminLoginParam){
+  
+  }
+  
   
   
 }
